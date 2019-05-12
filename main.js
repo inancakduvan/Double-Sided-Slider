6ac160969db766d279e0700aca8901f87c9ab72b
İnanç Akduvan <49209002+inancakduvan@users.noreply.github.com>
2019-05-12 17:57:49 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 function a(event) {
-    var btn = event.target.id;
-    var imagePosition = document.getElementById("imageBox" + btn + "").offsetLeft;
-    var contentPosition = document.getElementById("contentBox" + btn + "").offsetLeft;
-    var contentPositionMobile = document.getElementById("contentBox" + btn + "").offsetTop;
+  var btn = event.target.id;
+  var imagePosition = document.getElementById("imageBox" + btn + "").offsetLeft;
+  var contentPosition = document.getElementById("contentBox" + btn + "").offsetLeft;
+  var contentPositionMobile = document.getElementById("contentBox" + btn + "").offsetTop;
+
+  if (window.innerWidth > 1024) {
     document.getElementById("flexBoxImage").scrollTo({
-        top: 0,
-        left: imagePosition,
-        behavior: 'smooth'
-      });
-  
-      document.getElementById("flexBoxContent").scrollTo({
-        top: 0,
-        left: contentPosition,
-        behavior: 'smooth'
-      });
+      top: 0,
+      left: imagePosition,
+      behavior: 'smooth'
+    });
+
+    document.getElementById("flexBoxContent").scrollTo({
+      top: 0,
+      left: contentPosition,
+      behavior: 'smooth'
+    });
+  }
 
 
-      // MOBILE SCROLL
+  // MOBILE SCROLL
 
-      if(window.innerWidth <= 1024) {
-        document.getElementById("flexBoxContent").scrollTo({
-          top: contentPositionMobile,
-          left: 0,
-          behavior: 'smooth'
-        });
-      }
+  if (window.innerWidth <= 1024) {
+    console.log(contentPositionMobile)
+    document.getElementById("flexBoxContent").scrollTo({
+      top: contentPositionMobile,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
 
 }
